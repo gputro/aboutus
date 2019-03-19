a6e95a86fa27d23645455d077b38fb2d19c54110
gputro <gputro@indosoft.dc>
2019-03-19 05:05:02 -0400

    fourth commit

--- wp-content/themes/aboutus/index.php
+++ wp-content/themes/aboutus/index.php
@@ -40,6 +40,78 @@ get_header();
 				</div>
 				
 				</div>
+            <div class="why-us">
+                <div class="why-us-container">
+                    <div class="txt-why-us-left">
+                        <h1>Why alterra?</h1>
+                        <p>Alterra is connected to all telco players in Indonesia, and also licensed as the switching & aggregator for the services, processed millions of transactions every month, making it the best online telco aggregator in Indonesia.</p>
+                    </div>
+                </div>
+                <div class="img-why-us-right">
+                <img src="/wp-content/themes/aboutus/assets/img/about_funfact@2x.png" width="472" alt="Image" class="at-img-full">
+                </div>
+            </div>
+            <div class="our-team">
+                <div class="our-team-image-left">
+                     <img src="/wp-content/themes/aboutus/assets/img/content-6.png" width="472" alt="Image" class="at-img-full">
+                </div>
+                <div class="our-team-text-right">
+                        <h1>Our Team</h1>
+                        <p>Alterra team is 200 people strong (and counting!) with operations in Jakarta, Malang, and Jambi. Alterra was named the best work place in Indonesia by Jobplanet in 2017.</p>
+                </div>
+                
+            </div>
+          <div class="at-company mt-5 mb-5 aos-init aos-animate" data-aos="zoom-in">
+        <div class="container text-center">
+            <h5 class="font-weight-bold">Alterra is proud to partner with Indonesia’s top tech companies and build the country’s digital ecosystem together</h5>
+            <ul class="at-company--logo list-unstyled clearfix">
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo-tokopedia.png" alt="Tokopedia" width="92">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_bukalapak.png" alt="Bukalapak" width="104">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_traveloka.png" alt="Traveloka" width="120">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_shopee.png" alt="Shopee" width="94">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_kredivo.png" alt="Kredivo" width="81">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_xl.png" alt="XL" width="38">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_indosat.png" alt="Indosat" width="62">
+                            </a>
+                        </li>
+                                    
+            </ul>
+            <a href="https://www.alterra.id/contact-us/" class="at-fill--link">Get Started</a>
+        </div>
+    </div>
+                
 	</div>
 
 <?php
