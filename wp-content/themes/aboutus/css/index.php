46a76f2f790a768d6e355e44161942601a3c1672
gputro <gputro@indosoft.dc>
2019-04-08 05:16:19 -0400

    6 commit

---
+++ wp-content/themes/aboutus/css/index.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package aboutus
+ */
+
+get_header();
+?>
+	<div class="container-fluid">
+			<div class="containerutama">
+				<div class="container-left">
+                    <div class="isi-container">
+					<h1>Who is alterra?</h1>
+					<p>alterra is the leading B2B tech company in Indonesia that provides a wide range of services such as Bill Payment Aggregator, Distribution Network, Promotion Platform, and Tech Academy.</p>
+                    </div>
+				</div>
+				<div class="container-right">
+				<img src="/wp-content/themes/aboutus/assets/img/about_alterra@2x.png" width="420" alt="Image" class="at-img-full">
+                    <img class="batik" src="/wp-content/themes/aboutus/assets/img/batik2.svg" width="420" alt="Image" class="at-img-full">
+				</div>
+			</div>
+			<div class="milestone">
+				<div class="milestone-container">
+					<div class ="image-left">
+						<img src="/wp-content/themes/aboutus/assets/img/about_milestone@2x.png" width="472" alt="Image" class="at-img-full">
+						<div class="text-right">
+							<h1>Milestone</h1>
+							<p>The company has established since 2015 under the name Sepulsa, then it evolved as the pioneer of digital bill payment services, enabling top online players in Indonesia. It also served as technology enabler for offline to online (O2O) distribution network for Telco Industries, helping thousands of telco agents across Indonesia to get better use of technology for their business.</p>
+						</div>
+					</div>
+					
+				</div>
+				
+				</div>
+            <div class="why-us">
+                <div class="why-us-container">
+                    <div class="txt-why-us-left">
+                        <h1>Why alterra?</h1>
+                        <p>Alterra is connected to all telco players in Indonesia, and also licensed as the switching & aggregator for the services, processed millions of transactions every month, making it the best online telco aggregator in Indonesia.</p>
+                    </div>
+                </div>
+                <div class="img-why-us-right">
+                <img src="/wp-content/themes/aboutus/assets/img/about_funfact@2x.png" width="472" alt="Image" class="at-img-full">
+                </div>
+            </div>
+            <div class="our-team">
+                <div class="our-team-image-left">
+                     <img src="/wp-content/themes/aboutus/assets/img/content-6.png" width="472" alt="Image" class="at-img-full">
+                </div>
+                <div class="our-team-text-right">
+                        <h1>Our Team</h1>
+                        <p>Alterra team is 200 people strong (and counting!) with operations in Jakarta, Malang, and Jambi. Alterra was named the best work place in Indonesia by Jobplanet in 2017.</p>
+                </div>
+                
+            </div>
+          <div class="at-company mt-5 mb-5 aos-init aos-animate" data-aos="zoom-in">
+        <div class="container text-center">
+            <h5 class="font-weight-bold">Alterra is proud to partner with Indonesia’s top tech companies and build the country’s digital ecosystem together</h5>
+            <ul class="at-company--logo list-unstyled clearfix">
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo-tokopedia.png" alt="Tokopedia" width="92">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_bukalapak.png" alt="Bukalapak" width="104">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_traveloka.png" alt="Traveloka" width="120">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_shopee.png" alt="Shopee" width="94">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_kredivo.png" alt="Kredivo" width="81">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_xl.png" alt="XL" width="38">
+                            </a>
+                        </li>
+                            
+                        <li>
+                            <a href="">
+                                <img src="https://www.alterra.id/wp-content/uploads/2018/12/logo_indosat.png" alt="Indosat" width="62">
+                            </a>
+                        </li>
+                                    
+            </ul>
+            <a href="https://www.alterra.id/contact-us/" class="at-fill--link">Get Started</a>
+        </div>
+    </div>
+                
+	</div>
+
+<?php
+
+get_footer();
--- wp-content/themes/aboutus/footer.php
+++ wp-content/themes/aboutus/footer.php
@@ -17,11 +17,40 @@
     <div class="footer-link">
         <div class="container">
             <div class="row">
-            <div class="col-md-3 col-12">
-                gdfgdgdfgd
+            <div class="col-md-3 col-6">
+                    <div class="at-footer--logo">
+					<img src="https://www.alterra.id/wp-content/themes/alterra/assets/img/logo_white.png" alt="Image">
+						<span>© Copyright 2019</span>
+					</div> 
                 
                 </div>
-            
+                <div class="col-md-3 col-6">
+                <div class="at-footer--product">
+													<h3>Product</h3> 
+							<ul class="list-unstyled 0">
+																											<li>
+											<a href="https://www.alterra.id/bpa/">
+												Bill Payment Aggregator											</a>
+										</li>	
+																			<li>
+											<a href="https://www.alterra.id/distribution-network/">
+												Distribution Network											</a>
+										</li>	
+																			<li>
+											<a href="https://www.alterra.id/promotion-platform/">
+												Promotion Platform											</a>
+										</li>	
+																			<li>
+											<a href="https://www.alterra.id/tech-academy/">
+												Alterra Academy											</a>
+										</li>	
+										
+															</ul>
+																								<h3>Customers</h3> 
+							<ul class="list-unstyled 1">
+															</ul>
+																								</div>
+                </div>
             </div>    
         </div>
     </div>
